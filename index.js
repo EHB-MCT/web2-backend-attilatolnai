4cb6f9c9c788bc2e33d936e5b174136a0e21d8f5
attilatolnai <attila.tolnai@student.ehb.be>
2021-12-13 11:42:07 +0100

    Added API documentation
    Added an API documentation for better understanding

--- index.js
+++ index.js
@@ -2,26 +2,34 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+app.use(express.static('public'))
+
 app.get('/', (req, res) => {
-  res.send('Working on localhost!')
+  res.redirect('/info.html')
 })
 
-app.get('/data', (req, res) => {
+app.get('/dataMarket', (req, res) => {
+    //API data I want for each fleamarket
     let testMarket = {
-        //fleamarket
         name: "Fleamarket Brussel",
         location: "Bd Sylvain Dupuis 433, 1070 Brussels",
         date: "19/12/2021",
         time: "7h - 15h"
     }
-        //person
-        //pin_name: "lot 1",
-        //tags: "games, books, bike, clothes",
-        //description: "A lot of videogames, some children books, a kid sized bike and M sized clothes",
-        //pin_location: "50.83801363908659, 4.2867502898428445"
-
     res.send(testMarket)
-    //res.send('you are getting data!')
+    //res.send('you are getting data from testMarket!')
+  })
+
+  app.get('/dataPerson', (req, res) => {
+    //API data I want for each fleamarket
+    let testPerson = {
+        pin_name: "lot 1",
+        tags: "games, books, bike, clothes",
+        description: "A lot of videogames, some children books, a kid sized bike and M sized clothes",
+        pin_location: "50.83801363908659, 4.2867502898428445"
+    }
+    res.send(testPerson)
+    //res.send('you are getting data from testPerson!')
   })
 
 app.listen(port, () => {
