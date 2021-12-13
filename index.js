9714754b2ce04bf208868c123d7dc45dbdf51a05
attilatolnai <attila.tolnai@student.ehb.be>
2021-12-13 11:25:16 +0100

    testing getting data
    small test for get data. Thought about what data i need to put in my api

--- index.js
+++ index.js
@@ -6,6 +6,24 @@ app.get('/', (req, res) => {
   res.send('Working on localhost!')
 })
 
+app.get('/data', (req, res) => {
+    let testMarket = {
+        //fleamarket
+        name: "Fleamarket Brussel",
+        location: "Bd Sylvain Dupuis 433, 1070 Brussels",
+        date: "19/12/2021",
+        time: "7h - 15h"
+    }
+        //person
+        //pin_name: "lot 1",
+        //tags: "games, books, bike, clothes",
+        //description: "A lot of videogames, some children books, a kid sized bike and M sized clothes",
+        //pin_location: "50.83801363908659, 4.2867502898428445"
+
+    res.send(testMarket)
+    //res.send('you are getting data!')
+  })
+
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
 })
