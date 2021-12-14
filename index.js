bfff5f36d59d8c7be44b76819a68f622a82dff16
attilatolnai <attila.tolnai@student.ehb.be>
2021-12-14 15:54:01 +0100

    Connection to mongoDB
    Preparing to connect the backend with my database in mongoDB.

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
-const express = require('express')
-const bodyParser = require('body-parser')
+const express = require('express');
+const bodyParser = require('body-parser');
+const {MongoClient} = require('mongodb');
+require('dotenv').config();
 const app = express()
 const port = 3000
+
+const client =  new MongoClient(process.env.MONGO_URL);
+
+const app = express();
+const port = process.env.PORT || 3000;
 
 app.use(express.static('public'));
 app.use(bodyParser.json());
