13b4a183b96ea26a1210bd0aae87e8a87c3fb08c
attilatolnai <attila.tolnai@student.ehb.be>
2021-12-20 17:42:50 +0100

    Adding a new fleamarket to the database
    Trying to add a new fleamarket to the database with postman

--- index.js
+++ index.js
@@ -62,10 +62,45 @@ app.get('/dataMarket', async (req, res) => {
     //res.send('you are getting data from testPerson!')
   })
 
-  app.post('/saveMarket', (req,res) =>{
-    console.log(req.body);
-    res.send(`Data received with id: ${req.body._id}, name: ${req.body.name}, 
-    location: ${req.body.location}, date: ${req.body.date} and time: ${req.body.time} !`)
+  app.post('/saveMarket', async (req,res) =>{
+    if(!req.body._id || !req.body.name || !req.body.location || !req.body.date || !req.body.time){
+      res.status(400).send('Bad request: missing id, name, location, date or time');
+      return;
+    }
+    try{
+      await client.connect();
+       //retrieve all data from the collection "fleamarkets" from the database "courseProject"
+       const colli = client.db('courseProject').collection('fleamarkets');
+       //check if there isn't a fleamarket with the same name and location
+       const bg = await colli.findOne({name: req.body.name, location: req.body.location});
+       if(bg){
+         res.status(400).send(`Bad request: There is already a fleamarket with the name ${req.body.name} in ${req.body.location}`);
+         return;
+       }
+       //create a new fleamarket
+       let newMarket = {
+        name: req.body.name,
+        location: req.body.location,
+        date: req.body.date,
+        time: req.body.time
+       }
+       //Insert the new fleamarket into the database
+       let insertResult = await colli.insertOne(newMarket);
+       //Send back a successfull message if inserted successfully
+       res.status(201).json(newMarket);
+       return;
+    }catch(error){
+      console.log(error);
+      res.status(500).send({
+        error: 'Something went wrong',
+        value: error
+      });
+    }finally{
+      await client.close();
+    }
+    //console.log(req.body);
+    //res.send(`Data received with id: ${req.body._id}, name: ${req.body.name}, 
+    //location: ${req.body.location}, date: ${req.body.date} and time: ${req.body.time} !`)
   });
 
   app.post('/savePerson', (req,res) =>{
